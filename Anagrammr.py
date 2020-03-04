71e0e4c41c84f15d67664e342bd07f1a1d14cbb4
Aaron Lewis <aaron.lewis1011@gmail.com>
2020-03-05 09:57:19 +1100

    Updated readme, push final version

--- Anagrammr.py
+++ Anagrammr.py
@@ -1,29 +1,36 @@
+#Grab reference file, split each like to an array element, store as refList
 listFile = open("english3.txt", "r")
 refList = listFile.read().splitlines()
 resultList = []
+originalString = ""
 
-def permutations(string, step = 0):
-    if step == len(string):
-        # we've gotten to the end, print the permutation
+def permutations(string, step = 0): #Method for finding complete permutation of input, and comparing to reference list.
+    #when count reaches end of length of input word, add word to resultList
+    if step == len(string): 
         result = "".join(string)
         if result in refList and result not in resultList:
             resultList.append(result)
 
-    for i in range(step, len(string)):
-        # copy the string (store as array)
+    for i in range(step, len(string)): #Method for rearranging characters in input
+        # copy the string (store each character in array string_copy)
         string_copy = [c for c in string]
         # swap the current index with the step
         string_copy[step], string_copy[i] = string_copy[i], string_copy[step]
         # recurse on the portion of the string that has not been swapped yet
         permutations(string_copy, step + 1)
-
+    
 while True:
     userWord = input("Enter your word: ")
     if userWord.isalpha():
+        originalString = userWord
         break
     else:
         print("Enter a valid string.")
 permutations(userWord)
+if len(resultList) <= 1:
+    resultList.pop(0)
+    resultList.append("There were no anagrams in the reference list")
+
 print(resultList)
 
 
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Anagrammr
 Enter words, get anagrams. Simple.
+
+**CAUTION WITH ENTERING WORDS LONGER THAN 6 CHARS**
+
+Its going to hang... for a long time... unless you've got one of those gibsons, man.
